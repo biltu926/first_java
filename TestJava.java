15f28a2659fb562389acd7667dc745d8591643f8
Supratim Halder <supratim.halder@mindparabole.com>
2019-02-14 15:45:19 +0530

    A sample java to code

---
+++ TestJava.java
@@ -0,0 +1,10 @@
+
+class TestJava
+    {
+        public static void main(String args[]){
+            System.out.println("Supratim halder");
+            String s = "supratim";
+
+            System.out.println(s.substring(1,4));
+        }
+    }
